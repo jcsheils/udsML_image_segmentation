19ede2a36c0cba91a940fa6613e9de67e003618f
Muhammad Umar Javed <muja00003@stud.uni-saarland.de>
2025-08-29 22:09:57 +0500

    Del G: Add GrabCutSegmenter which uses cv2.grabCut

--- segmenters.py
+++ segmenters.py
@@ -26,3 +26,49 @@ class KNNSegmenter:
         
         return pred
 
+class GrabCutSegmenter:
+    def __init__(self, iter_count: int = 3, k = None):
+        self.iter_count = iter_count
+
+    def train(self, images_train, scrib_train, gt_train):
+        pass
+
+    def infer(self, images, scrib, only_connected_components=False):
+        pred = []
+        for image, scribble in tqdm.tqdm(zip(images, scrib), total=len(list(zip(images, scrib)))):
+
+            gc_mask = np.full(scribble.shape, cv2.GC_PR_BGD, dtype=np.uint8)
+
+            # Idea 1
+
+            # y_coords, x_coords = np.where(scribble == 1)
+            # if y_coords.size > 0 and x_coords.size > 0:
+            #     x_min, x_max = x_coords.min(), x_coords.max()
+            #     y_min, y_max = y_coords.min(), y_coords.max()
+            #     gc_mask[y_min:y_max+1, x_min:x_max+1] = cv2.GC_PR_FGD
+
+            # Idea 2
+            # fg_scribble_mask = (scribble == 1).astype(np.uint8)
+            # num_labels, labels = cv2.connectedComponents(fg_scribble_mask)
+
+            # for i in range(1, num_labels):
+            #     component_mask = (labels == i)
+            #     y_coords, x_coords = np.where(component_mask)
+                
+            #     if y_coords.size > 0 and x_coords.size > 0:
+            #         x_min, x_max = x_coords.min(), x_coords.max()
+            #         y_min, y_max = y_coords.min(), y_coords.max()
+            #         gc_mask[y_min:y_max+1, x_min:x_max+1] = cv2.GC_PR_FGD
+
+            gc_mask[scribble == 0] = cv2.GC_BGD
+            gc_mask[scribble == 1] = cv2.GC_FGD
+
+            bgdModel = np.zeros((1, 65), np.float64)
+            fgdModel = np.zeros((1, 65), np.float64)
+
+            cv2.grabCut(image, gc_mask, None, bgdModel, fgdModel, self.iter_count, cv2.GC_INIT_WITH_MASK)
+            
+            output_mask = np.where((gc_mask == cv2.GC_PR_FGD) | (gc_mask == cv2.GC_FGD), 1, 0).astype(np.uint8)
+            pred.append(output_mask)
+            
+        return pred
